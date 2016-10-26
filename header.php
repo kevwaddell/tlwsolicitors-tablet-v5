1ed0ee1dc569c3e791178a87979d94519734db88
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-10-26 11:32:42 +0100

    Add tag manager code for staging site.

--- header.php
+++ header.php
@@ -1,6 +1,12 @@
 <?php include (STYLESHEETPATH . '/_/inc/global/head-html.php'); ?>	
 
 <body <?php body_class(); ?>>
+<?php if ($_SERVER['SERVER_NAME']=='tlwsolicitors.staging.wpengine.com') { ?>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PDNR9J"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<?php } ?>
 <?php if ($_SERVER['SERVER_NAME']=='www.tlwsolicitors.co.uk') { ?>
 <!-- Google Tag Manager -->
 <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PLBR4F"
