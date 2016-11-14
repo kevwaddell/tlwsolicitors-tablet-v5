9f64fce3e66fe2fe07da06280e1a8a5a23c52e37
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-11-14 10:32:03 +0000

    Fix google tag manager script placing.

--- parts/headers/header-landing-page.php
+++ parts/headers/header-landing-page.php
@@ -1,15 +1,16 @@
 <?php include (STYLESHEETPATH . '/_/inc/landing-page/head-html.inc'); ?>	
 
 <body id="landing-page" <?php body_class(); ?>>
+<?php if ($_SERVER['SERVER_NAME']=='tlwsolicitors.staging.wpengine.com') { ?>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PDNR9J"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<?php } ?>
 <?php if ($_SERVER['SERVER_NAME']=='www.tlwsolicitors.co.uk') { ?>
 <!-- Google Tag Manager -->
 <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PLBR4F"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PLBR4F');</script>
 <!-- End Google Tag Manager -->
 <?php } ?>
 
