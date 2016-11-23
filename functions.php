b61b4fee9a2604e76b13438a4f0aed8225602fa4
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-11-23 11:15:36 +0000

    Fix bootstrap on login page.

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ add_action( 'after_setup_theme', 'editor_styles' );
 
 function tlw_scripts() {
 	// Load stylesheets.
-	wp_enqueue_style( 'styles', get_stylesheet_directory_uri().'/_/css/styles.css', array('twitter-bootstrap', 'A2A_SHARE_SAVE', 'gforms_css'), filemtime( get_stylesheet_directory().'/_/css/styles.css' ), 'screen' );
+	wp_enqueue_style( 'styles', get_stylesheet_directory_uri().'/_/css/styles.css', array('twitter-bootstrap'), filemtime( get_stylesheet_directory().'/_/css/styles.css' ), 'screen' );
 	
 	// Load JS
 	//wp_enqueue_script( 'jquery' );
