626161948f50dee8af654fb2c32f125d9b835583
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2017-01-20 14:23:25 +0000

    Deregister css forms for gravity forms

--- functions.php
+++ functions.php
@@ -99,9 +99,6 @@ add_filter('script_loader_tag', 'add_async_attribute', 10, 2);
 function custom_dequeue() {
     wp_dequeue_style('autoptimize-toolbar');
     wp_deregister_style('autoptimize-toolbar');
-    
-    wp_dequeue_style('gforms_css');
-    wp_deregister_style('gforms_css');
 }
 
 add_action( 'wp_enqueue_scripts', 'custom_dequeue', 9999 );
